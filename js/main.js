2501725d809ff122332824362bfcd9a06f1c112f
fmfahath <fmfahath.dev@gmail.com>
2023-02-02 23:29:06 +0530

    br_responsive/js/852px : navbar menuicon button toggle function added

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ var swiper = new Swiper(".home", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  // Navbar show & hide
+  let menu = document.querySelector('#menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+  };
+
+  window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
